aadcb7851d24025de6725a2ddf27f78f42be590e
Jakub Zięba <jakubzieba7@gmail.com>
2023-03-04 10:22:09 +0100

    Add project files.

---
+++ ProgramKadrowy/AddEmployee.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace ProgramKadrowy
+{
+    partial class AddEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbEmployeeID = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.tbSalary = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblWorkTemination = new System.Windows.Forms.Label();
+            this.rtbRemarks = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dtpWorkTermination = new System.Windows.Forms.DateTimePicker();
+            this.dtpHireDate = new System.Windows.Forms.DateTimePicker();
+            this.cbAgreementType = new System.Windows.Forms.ComboBox();
+            this.cbIsActiveEmployee = new System.Windows.Forms.CheckBox();
+            this.btExit = new System.Windows.Forms.Button();
+            this.btConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbEmployeeID
+            // 
+            this.tbEmployeeID.Location = new System.Drawing.Point(123, 12);
+            this.tbEmployeeID.Name = "tbEmployeeID";
+            this.tbEmployeeID.Size = new System.Drawing.Size(146, 20);
+            this.tbEmployeeID.TabIndex = 0;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(123, 50);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(146, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(123, 81);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(146, 20);
+            this.tbSurname.TabIndex = 2;
+            // 
+            // tbSalary
+            // 
+            this.tbSalary.Location = new System.Drawing.Point(123, 118);
+            this.tbSalary.Name = "tbSalary";
+            this.tbSalary.Size = new System.Drawing.Size(146, 20);
+            this.tbSalary.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Imię";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nazwisko";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Wysokość zarobków";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Data zatrudnienia";
+            // 
+            // lblWorkTemination
+            // 
+            this.lblWorkTemination.AllowDrop = true;
+            this.lblWorkTemination.AutoSize = true;
+            this.lblWorkTemination.Location = new System.Drawing.Point(11, 383);
+            this.lblWorkTemination.Name = "lblWorkTemination";
+            this.lblWorkTemination.Size = new System.Drawing.Size(151, 13);
+            this.lblWorkTemination.TabIndex = 9;
+            this.lblWorkTemination.Text = "Data zakończenia współpracy";
+            // 
+            // rtbRemarks
+            // 
+            this.rtbRemarks.Location = new System.Drawing.Point(14, 267);
+            this.rtbRemarks.Name = "rtbRemarks";
+            this.rtbRemarks.Size = new System.Drawing.Size(255, 96);
+            this.rtbRemarks.TabIndex = 10;
+            this.rtbRemarks.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Typ umowy";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Uwagi";
+            // 
+            // dtpWorkTermination
+            // 
+            this.dtpWorkTermination.Location = new System.Drawing.Point(14, 415);
+            this.dtpWorkTermination.Name = "dtpWorkTermination";
+            this.dtpWorkTermination.Size = new System.Drawing.Size(255, 20);
+            this.dtpWorkTermination.TabIndex = 13;
+            // 
+            // dtpHireDate
+            // 
+            this.dtpHireDate.Location = new System.Drawing.Point(123, 154);
+            this.dtpHireDate.Name = "dtpHireDate";
+            this.dtpHireDate.Size = new System.Drawing.Size(146, 20);
+            this.dtpHireDate.TabIndex = 14;
+            // 
+            // cbAgreementType
+            // 
+            this.cbAgreementType.FormattingEnabled = true;
+            this.cbAgreementType.Location = new System.Drawing.Point(123, 192);
+            this.cbAgreementType.Name = "cbAgreementType";
+            this.cbAgreementType.Size = new System.Drawing.Size(146, 21);
+            this.cbAgreementType.TabIndex = 15;
+            // 
+            // cbIsActiveEmployee
+            // 
+            this.cbIsActiveEmployee.AutoSize = true;
+            this.cbIsActiveEmployee.CheckAlign = System.Drawing.ContentAlignment.TopRight;
+            this.cbIsActiveEmployee.Checked = true;
+            this.cbIsActiveEmployee.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbIsActiveEmployee.Location = new System.Drawing.Point(151, 235);
+            this.cbIsActiveEmployee.Name = "cbIsActiveEmployee";
+            this.cbIsActiveEmployee.Size = new System.Drawing.Size(118, 17);
+            this.cbIsActiveEmployee.TabIndex = 16;
+            this.cbIsActiveEmployee.Text = "Aktywny pracownik";
+            this.cbIsActiveEmployee.UseVisualStyleBackColor = true;
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(87, 460);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(75, 23);
+            this.btExit.TabIndex = 17;
+            this.btExit.Text = "Wyjdź";
+            this.btExit.UseVisualStyleBackColor = true;
+            // 
+            // btConfirm
+            // 
+            this.btConfirm.Location = new System.Drawing.Point(194, 460);
+            this.btConfirm.Name = "btConfirm";
+            this.btConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btConfirm.TabIndex = 18;
+            this.btConfirm.Text = "Zatwierdź";
+            this.btConfirm.UseVisualStyleBackColor = true;
+            // 
+            // AddEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 500);
+            this.Controls.Add(this.btConfirm);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.cbIsActiveEmployee);
+            this.Controls.Add(this.cbAgreementType);
+            this.Controls.Add(this.dtpHireDate);
+            this.Controls.Add(this.dtpWorkTermination);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.rtbRemarks);
+            this.Controls.Add(this.lblWorkTemination);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSalary);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbEmployeeID);
+            this.Name = "AddEmployee";
+            this.Text = "Dodawanie pracownika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbEmployeeID;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.TextBox tbSalary;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblWorkTemination;
+        private System.Windows.Forms.RichTextBox rtbRemarks;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dtpWorkTermination;
+        private System.Windows.Forms.DateTimePicker dtpHireDate;
+        private System.Windows.Forms.ComboBox cbAgreementType;
+        private System.Windows.Forms.CheckBox cbIsActiveEmployee;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.Button btConfirm;
+    }
+}
---
+++ ProgramKadrowy/Main.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ProgramKadrowy
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEmployeesGrid = new System.Windows.Forms.DataGridView();
+            this.btAddEmployee = new System.Windows.Forms.Button();
+            this.btEditEmployee = new System.Windows.Forms.Button();
+            this.btRefreshGridView = new System.Windows.Forms.Button();
+            this.cbSortList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeesGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEmployeesGrid
+            // 
+            this.dgvEmployeesGrid.AllowUserToDeleteRows = false;
+            this.dgvEmployeesGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvEmployeesGrid.BackgroundColor = System.Drawing.Color.White;
+            this.dgvEmployeesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployeesGrid.Location = new System.Drawing.Point(12, 55);
+            this.dgvEmployeesGrid.Name = "dgvEmployeesGrid";
+            this.dgvEmployeesGrid.Size = new System.Drawing.Size(854, 424);
+            this.dgvEmployeesGrid.TabIndex = 0;
+            // 
+            // btAddEmployee
+            // 
+            this.btAddEmployee.Location = new System.Drawing.Point(13, 13);
+            this.btAddEmployee.Name = "btAddEmployee";
+            this.btAddEmployee.Size = new System.Drawing.Size(75, 23);
+            this.btAddEmployee.TabIndex = 1;
+            this.btAddEmployee.Text = "Dodaj";
+            this.btAddEmployee.UseVisualStyleBackColor = true;
+            // 
+            // btEditEmployee
+            // 
+            this.btEditEmployee.Location = new System.Drawing.Point(126, 13);
+            this.btEditEmployee.Name = "btEditEmployee";
+            this.btEditEmployee.Size = new System.Drawing.Size(75, 23);
+            this.btEditEmployee.TabIndex = 2;
+            this.btEditEmployee.Text = "Edytuj";
+            this.btEditEmployee.UseVisualStyleBackColor = true;
+            // 
+            // btRefreshGridView
+            // 
+            this.btRefreshGridView.Location = new System.Drawing.Point(245, 13);
+            this.btRefreshGridView.Name = "btRefreshGridView";
+            this.btRefreshGridView.Size = new System.Drawing.Size(75, 23);
+            this.btRefreshGridView.TabIndex = 3;
+            this.btRefreshGridView.Text = "Odśwież";
+            this.btRefreshGridView.UseVisualStyleBackColor = true;
+            // 
+            // cbSortList
+            // 
+            this.cbSortList.FormattingEnabled = true;
+            this.cbSortList.Location = new System.Drawing.Point(457, 12);
+            this.cbSortList.Name = "cbSortList";
+            this.cbSortList.Size = new System.Drawing.Size(121, 21);
+            this.cbSortList.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(381, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Sortowanie";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(886, 524);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbSortList);
+            this.Controls.Add(this.btRefreshGridView);
+            this.Controls.Add(this.btEditEmployee);
+            this.Controls.Add(this.btAddEmployee);
+            this.Controls.Add(this.dgvEmployeesGrid);
+            this.Name = "Main";
+            this.Text = "Program kadrowy";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeesGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmployeesGrid;
+        private System.Windows.Forms.Button btAddEmployee;
+        private System.Windows.Forms.Button btEditEmployee;
+        private System.Windows.Forms.Button btRefreshGridView;
+        private System.Windows.Forms.ComboBox cbSortList;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
